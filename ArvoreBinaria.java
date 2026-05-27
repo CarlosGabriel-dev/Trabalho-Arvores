029890990fafea3b629bd27724949b5a34b121ab
Eduardo <eduardokuhnen@edu.unifil.br>
2026-05-27 17:41:33 -0300

    Versão Final Corrigida

--- ArvoreBinaria.java
+++ ArvoreBinaria.java
@@ -1,274 +1,357 @@
 public class ArvoreBinaria {
-    No raiz;
+   No raiz;
 
-    public ArvoreBinaria() {
 
-        this.raiz = new No(null);
-        System.out.println("Árvore criada com sucesso");
+   public ArvoreBinaria() {
 
-    }
 
-    public void inserir(Integer conteudo) {
+       this.raiz = new No(null);
+   }
 
-        No novoNo = new No(conteudo);
 
-        if(estaVazia()) {
-            this.raiz = novoNo;
-            System.out.println("Raiz criada com sucesso com valor: " + novoNo.getConteudo());
-        } else {
-            inserirRecursivo(novoNo, this.raiz);
-        }
-    }
+   public void inserir(Integer conteudo) {
 
-    public void inserirRecursivo(No novoNo, No aux) {
 
-        if(aux.getConteudo() > novoNo.getConteudo()) {
+       No novoNo = new No(conteudo);
 
-            if(aux.getEsquerda() == null) {
-                aux.setEsquerda(novoNo);
-                System.out.println("Nó " + novoNo.getConteudo() + " inserido com sucesso.");
-                return;
-            } else {
-                inserirRecursivo(novoNo, aux.getEsquerda());
-            }
-        } else if (aux.getConteudo() < novoNo.getConteudo()) {
-            if(aux.getDireita() == null) {
-                aux.setDireita(novoNo);
-                System.out.println("Nó " + novoNo.getConteudo() + " inserido com sucesso.");
-                return;
-            } else {
-                inserirRecursivo(novoNo, aux.getDireita());
-            }
-        } else {
-            System.out.println("Não são permitidos nós repetidos na árvore binária. O " + novoNo.getConteudo() + " já existe na árvore.");
-            return;
-        }
-    }
 
-    private boolean estaVazia () {
+       if(estaVazia()) {
+           this.raiz = novoNo;
+           System.out.println("Raiz criada com sucesso com valor: " + novoNo.getConteudo());
+       } else {
+           inserirRecursivo(novoNo, this.raiz);
+       }
+   }
 
-    if(this.raiz.getConteudo() == null) {
-        return true;
-    } else {
-        return false;
-    }
+
+   public void inserirRecursivo(No novoNo, No aux) {
+
+
+       if(aux.getConteudo() > novoNo.getConteudo()) {
+
+
+           if(aux.getEsquerda() == null) {
+               aux.setEsquerda(novoNo);
+               System.out.println("Nó " + novoNo.getConteudo() + " inserido com sucesso.");
+               return;
+           } else {
+               inserirRecursivo(novoNo, aux.getEsquerda());
+           }
+       } else if (aux.getConteudo() < novoNo.getConteudo()) {
+           if(aux.getDireita() == null) {
+               aux.setDireita(novoNo);
+               System.out.println("Nó " + novoNo.getConteudo() + " inserido com sucesso.");
+               return;
+           } else {
+               inserirRecursivo(novoNo, aux.getDireita());
+           }
+       } else {
+           System.out.println("Não são permitidos nós repetidos na árvore binária. O " + novoNo.getConteudo() + " já existe na árvore.");
+           return;
+       }
+   }
+
+
+   private boolean estaVazia () {
+
+
+   if(this.raiz.getConteudo() == null) {
+       return true;
+   } else {
+       return false;
+   }
 }
 
-    public void percurso(String percurso) {
 
-        if(estaVazia()) {
-            System.out.println("A árvore não existe.");
-            return;
-        }
-        
-        switch (percurso) {
-            case("Pre"):
-                System.out.println("Executando a árvore em pré ordem.");
-                this.preOrdem(this.raiz);
-                break;
-            case("Em"):
-                System.out.println("Executando a árvore em ordem.");
-                this.emOrdem(this.raiz);
-                break;
-            case("Pos"):
-                System.out.println("Executando a árvore em pós ordem.");
-                this.posOrdem(this.raiz);
-                break;
-            default:
-                System.out.println("Percurso inexistente!");
-                break;
-        }
-    }
+   public void percurso(String percurso) {
 
-    private void posOrdem(No no) {
-        if(no == null) {
-            return;
-        }
-        posOrdem(no.getEsquerda());
-        posOrdem(no.getDireita());
-        System.out.println(no.getConteudo());
-    }
 
-    private void preOrdem(No no) {
-        if(no == null) {
-            return;
-        }
-        System.out.println(no.getConteudo());
-        preOrdem(no.getEsquerda());
-        preOrdem(no.getDireita());
-    }
+       if(estaVazia()) {
+           System.out.println("A árvore não existe.");
+           return;
+       }
+      
+       switch (percurso) {
+           case("Pre"):
+               System.out.println("Executando a árvore em pré ordem.");
+               this.preOrdem(this.raiz);
+               break;
+           case("Em"):
+               System.out.println("Executando a árvore em ordem.");
+               this.emOrdem(this.raiz);
+               break;
+           case("Pos"):
+               System.out.println("Executando a árvore em pós ordem.");
+               this.posOrdem(this.raiz);
+               break;
+           default:
+               System.out.println("Percurso inexistente!");
+               break;
+       }
+   }
 
-    private void emOrdem(No no) {
-        if(no == null) {
-            return;
-        }
-        emOrdem(no.getEsquerda());
-        System.out.println(no.getConteudo());
-        emOrdem(no.getDireita());
-    }
 
-    public void remover(Integer conteudo) {
-        if(estaVazia()) {
-            System.out.println("A árvore não existe.");
-            return;
-        }
+   private void posOrdem(No no) {
+       if(no == null) {
+           return;
+       }
+       posOrdem(no.getEsquerda());
+       posOrdem(no.getDireita());
+       System.out.println(no.getConteudo());
+   }
 
-        No noEncontrado = buscarNo(conteudo, this.raiz);
 
-        if(noEncontrado != null) {
-            identificarTipo(noEncontrado);
-        } else {
-            System.out.println("Este valor não está presente na árvore!");
-        }
-    }
+   private void preOrdem(No no) {
+       if(no == null) {
+           return;
+       }
+       System.out.println(no.getConteudo());
+       preOrdem(no.getEsquerda());
+       preOrdem(no.getDireita());
+   }
 
-    private No buscarNo(Integer conteudo, No no) {
 
-        // chegou em um nó vazio
-        if(no == null) {
-            return null;
-        }
+   private void emOrdem(No no) {
+       if(no == null) {
+           return;
+       }
+       emOrdem(no.getEsquerda());
+       System.out.println(no.getConteudo());
+       emOrdem(no.getDireita());
+   }
 
-        // encontrou o valor
-        if(conteudo.equals(no.getConteudo())) {
-            return no;
-        }
 
-        // procura na esquerda
-        if(conteudo < no.getConteudo()) {
-            return buscarNo(conteudo, no.getEsquerda());
-        }
+   public void remover(Integer conteudo) {
+       if(estaVazia()) {
+           System.out.println("A árvore não existe.");
+           return;
+       }
 
-        // procura na direita
-        return buscarNo(conteudo, no.getDireita());
-    }
 
-    private void identificarTipo(No no){
+       No noEncontrado = buscarNo(conteudo, this.raiz);
 
-        if(no.getEsquerda() == null && no.getDireita() == null){
-            removerNoFolha(no);
-        } 
 
-        else if((no.getEsquerda() != null && no.getDireita() == null) || (no.getEsquerda() == null && no.getDireita() != null)){
-            removerNoFilho(no);
-        } 
+       if(noEncontrado != null) {
+           identificarTipo(noEncontrado);
+       } else {
+           System.out.println("Este valor não está presente na árvore!");
+       }
+   }
 
-        else if(no.getEsquerda() != null && no.getDireita() != null){
-            removerNoDoisFilhos(no);
-        } 
-        
-        else {
-            System.out.println("Este valor não está presente na árvore!");
-        }
-    }
 
-    public void buscarEIdentificar(Integer conteudo) {
+   private No buscarNo(Integer conteudo, No no) {
 
-        No noEncontrado = buscarNo(conteudo, raiz);
 
-        if(noEncontrado != null) {
-            identificarTipo(noEncontrado);
-        } 
-        
-        else {
-            System.out.println("Este valor não está presente na árvore!");
-        }
-    }
+       // chegou em um nó vazio
+       if(no == null) {
+           return null;
+       }
 
-    //REMOVE NO FOLHA (NO QUE NÃO TENHA FILHOS)
-    private void removerNoFolha(No no) {
 
-        System.out.println("O nó " + no.getConteudo() + " é um nó folha.");
+       // encontrou o valor
+       if(conteudo.equals(no.getConteudo())) {
+           return no;
+       }
 
-        // REMOVE A RAIZ
-        if(no == raiz) {
-            raiz = new No(null);
-            return;
-        }
 
-        No pai = raiz;
-        No atual = raiz;
+       // procura na esquerda
+       if(conteudo < no.getConteudo()) {
+           return buscarNo(conteudo, no.getEsquerda());
+       }
 
-        // PROCURA O NÓ E GUARDA O PAI
-        while(atual != null && atual != no) {
 
-            pai = atual;
+       // procura na direita
+       return buscarNo(conteudo, no.getDireita());
+   }
 
-            if(no.getConteudo() < atual.getConteudo()) {
-                atual = atual.getEsquerda();
-            } else {
-                atual = atual.getDireita();
-            }
-        }
 
-        // REMOVE O NÓ
-        if(pai.getEsquerda() == no) {
-            pai.setEsquerda(null);
-        } else {
-            pai.setDireita(null);
-        }
-    }
+   private void identificarTipo(No no){
 
-    //REMOVE UM NO QUE POSSUI APENAS UM FILHO
-    private void removerNoFilho(No no){
 
-        System.out.println("O nó " + no.getConteudo() + " possui um filho.");
-        No filho;
+       if(no.getEsquerda() == null && no.getDireita() == null){
+           removerNoFolha(no);
+       }
 
-        // DESCOBRE QUAL É O FILHO
-        if(no.getEsquerda() != null) {
-            filho = no.getEsquerda();
-        } else {
-            filho = no.getDireita();
-        }
 
-        // REMOVE A RAIZ
-        if(no == raiz) {
-            raiz = filho;
-            return;
-        }
+       else if((no.getEsquerda() != null && no.getDireita() == null) || (no.getEsquerda() == null && no.getDireita() != null)){
+           removerNoFilho(no);
+       }
 
-        No pai = raiz;
-        No atual = raiz;
 
-        // PROCURA O NÓ E GUARDA O PAI
-        while(atual != null && atual != no) {
+       else if(no.getEsquerda() != null && no.getDireita() != null){
+           removerNoDoisFilhos(no);
+       }
+      
+       else {
+           System.out.println("Este valor não está presente na árvore!");
+       }
+   }
 
-            pai = atual;
 
-            if(no.getConteudo() < atual.getConteudo()) {
-                atual = atual.getEsquerda();
-            } else {
-                atual = atual.getDireita();
-            }
-        }
+   public void buscarEIdentificar(Integer conteudo) {
 
-        // LIGA O PAI DIRETAMENTE AO FILHO
-        if(pai.getEsquerda() == no) {
-            pai.setEsquerda(filho);
-        } else {
-            pai.setDireita(filho);
-        }
-    }
 
-    //INDENTIFICA E REMOVE NO COM DOIS FILHOS
-    private void removerNoDoisFilhos(No no) {
-        System.out.println("O nó " + no.getConteudo() + " é um nó com dois filhos.");
-        if(no.getEsquerda().getDireita() == null) {
-            no.setConteudo(no.getEsquerda().getConteudo());
-            no.setEsquerda(no.getEsquerda().getEsquerda());
-        } else {
-            No sucessor = no.getEsquerda();
-            No antecessor = sucessor;
-            while(sucessor.getDireita() != null) {
-                antecessor = sucessor;
-                sucessor = sucessor.getDireita();
-            }
-            no.setConteudo(sucessor.getConteudo());
-            if(antecessor.getDireita() == sucessor) {
-                antecessor.setDireita(sucessor.getEsquerda());
-            }
-            antecessor.setDireita(sucessor.getEsquerda());
-        }
-    }
+       No noEncontrado = buscarNo(conteudo, raiz);
+
+
+       if(noEncontrado != null) {
+           identificarTipo(noEncontrado);
+       }
+      
+       else {
+           System.out.println("Este valor não está presente na árvore!");
+       }
+   }
+
+
+   //REMOVE NO FOLHA (NO QUE NÃO TENHA FILHOS)
+   private void removerNoFolha(No no) {
+
+
+       System.out.println("O nó " + no.getConteudo() + " é um nó folha.");
+
+
+       // REMOVE A RAIZ
+       if(no == raiz) {
+           raiz = new No(null);
+           return;
+       }
+
+
+       No pai = raiz;
+       No atual = raiz;
+
+
+       // PROCURA O NÓ E GUARDA O PAI
+       while(atual != null && atual != no) {
+
+
+           pai = atual;
+
+
+           if(no.getConteudo() < atual.getConteudo()) {
+               atual = atual.getEsquerda();
+           } else {
+               atual = atual.getDireita();
+           }
+       }
+
+
+       // REMOVE O NÓ
+       if(pai.getEsquerda() == no) {
+           pai.setEsquerda(null);
+       } else {
+           pai.setDireita(null);
+       }
+   }
+
+
+   //REMOVE UM NO QUE POSSUI APENAS UM FILHO
+   private void removerNoFilho(No no){
+
+
+       System.out.println("O nó " + no.getConteudo() + " possui um filho.");
+       No filho;
+
+
+       // DESCOBRE QUAL É O FILHO
+       if(no.getEsquerda() != null) {
+           filho = no.getEsquerda();
+       } else {
+           filho = no.getDireita();
+       }
+
+
+       // REMOVE A RAIZ
+       if(no == raiz) {
+           raiz = filho;
+           return;
+       }
+
+
+       No pai = raiz;
+       No atual = raiz;
+
+
+       // PROCURA O NÓ E GUARDA O PAI
+       while(atual != null && atual != no) {
+
+
+           pai = atual;
+
+
+           if(no.getConteudo() < atual.getConteudo()) {
+               atual = atual.getEsquerda();
+           } else {
+               atual = atual.getDireita();
+           }
+       }
+
+
+       // LIGA O PAI DIRETAMENTE AO FILHO
+       if(pai.getEsquerda() == no) {
+           pai.setEsquerda(filho);
+       } else {
+           pai.setDireita(filho);
+       }
+   }
+
+
+   //INDENTIFICA E REMOVE NO COM DOIS FILHOS
+   private void removerNoDoisFilhos(No no) {
+       System.out.println("O nó " + no.getConteudo() + " é um nó com dois filhos.");
+       if(no.getEsquerda().getDireita() == null) {
+           no.setConteudo(no.getEsquerda().getConteudo());
+           no.setEsquerda(no.getEsquerda().getEsquerda());
+       } else {
+           No sucessor = no.getEsquerda();
+           No antecessor = sucessor;
+           while(sucessor.getDireita() != null) {
+               antecessor = sucessor;
+               sucessor = sucessor.getDireita();
+           }
+           no.setConteudo(sucessor.getConteudo());
+           if(antecessor.getDireita() == sucessor) {
+               antecessor.setDireita(sucessor.getEsquerda());
+           }
+           antecessor.setDireita(sucessor.getEsquerda());
+       }
+   }
+
+
+
+
+   //VISUALIZAÇÃO DE ARVORE VIA TERMINAL
+   public void imprimirArvore() {
+       if (estaVazia()) {
+           System.out.println("(árvore vazia)");
+           return;
+       }
+
+
+       java.util.Queue<No> fila = new java.util.LinkedList<>();
+       fila.add(this.raiz);
+       int nivel = 0;
+
+
+       while (!fila.isEmpty()) {
+           int tamanho = fila.size();
+           System.out.print("Nível " + nivel + (nivel == 0 ? " (Raiz): " : ": "));
+
+
+           for (int i = 0; i < tamanho; i++) {
+               No atual = fila.poll();
+               System.out.print(atual.getConteudo());
+               if (i < tamanho - 1) System.out.print(", ");
+
+
+               if (atual.getEsquerda() != null) fila.add(atual.getEsquerda());
+               if (atual.getDireita() != null) fila.add(atual.getDireita());
+           }
+
+
+           System.out.println();
+           nivel++;
+       }
+   }
 }
--- Main.java
+++ Main.java
@@ -1,50 +1,105 @@
 import java.util.Scanner;
 
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArvoreBinaria arvore = new ArvoreBinaria();
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       ArvoreBinaria arvore = new ArvoreBinaria();
 
-        while (true) {
-            System.out.println("\n===== MENU =====");
-            System.out.println("1 - Inserir");
-            System.out.println("2 - Remover");
-            System.out.println("3 - Percurso");
-            System.out.println("4 - Nova arvore");
-            System.out.println("5 - Sair");
-            System.out.print("Escolha: ");
 
-            int opcao = sc.nextInt();
+       while (true) {
+           System.out.println("\n===== MENU =====");
+           System.out.println("1. Inserir valores");
+           System.out.println("2. Remover valor");
+           System.out.println("3. Percurso");
+           System.out.println("4. Nova arvore");
+           System.out.println("5. Visualizar arvore");
+           System.out.println("6. Sair");
+           System.out.print("Escolha uma opcao: ");
 
-            switch (opcao) {
-                case 1:
-                    System.out.print("Valor: ");
-                    arvore.inserir(sc.nextInt());
-                    break;
 
-                case 2:
-                    System.out.print("Valor: ");
-                    arvore.remover(sc.nextInt());
-                    break;
+           String opcao = sc.nextLine().trim();
 
-                case 3:
-                    System.out.println("Qual percurso? (Pre / Em / Pos)");
-                    arvore.percurso(sc.next());
-                    break;
 
-                case 4:
-                    arvore = new ArvoreBinaria();
-                    System.out.println("Nova arvore criada!");
-                    break;
+           switch (opcao) {
+               case "1":
+                   System.out.print("Digite os valores para inserir (ex: 10,20,30): ");
+                   String entradaInsercao = sc.nextLine().trim();
+                   String[] valores = entradaInsercao.split(",");
 
-                case 5:
-                    System.out.println("Ate mais!");
-                    sc.close();
-                    return;
 
-                default:
-                    System.out.println("Opcao invalida.");
-            }
-        }
-    }
+                   for (String val : valores) {
+                       arvore.inserir(Integer.parseInt(val.trim()));
+                   }
+
+
+                   if (valores.length == 1) {
+                       System.out.println("\nArvore apos inserir " + entradaInsercao.trim() + ":");
+                   } else {
+                       System.out.println("\nArvore montada:");
+                   }
+
+
+                   arvore.imprimirArvore();
+                   break;
+
+
+               case "2":
+                   System.out.print("Digite os valores para remover (ex: 10,20): ");
+                   String entradaRemocao = sc.nextLine().trim();
+
+
+                   for (String val : entradaRemocao.split(",")) {
+                       int valorRemover = Integer.parseInt(val.trim());
+                       arvore.remover(valorRemover);
+                       System.out.println("\nArvore apos remover " + valorRemover + ":");
+                       arvore.imprimirArvore();
+                   }
+                   break;
+
+
+               case "3":
+                   System.out.print("Qual percurso? (Pre / Em / Pos): ");
+                   arvore.percurso(sc.nextLine().trim());
+                   break;
+
+
+               case "4":
+                   System.out.print("Deseja iniciar uma nova arvore? (sim / nao): ");
+                   if (sc.nextLine().trim().equalsIgnoreCase("sim")) {
+                       arvore = new ArvoreBinaria();
+                       System.out.print("Digite os valores da nova arvore (ex: 10,20,30): ");
+                       String entradaNova = sc.nextLine().trim();
+                       String[] valoresNovos = entradaNova.split(",");
+
+
+                       for (String val : valoresNovos) {
+                           arvore.inserir(Integer.parseInt(val.trim()));
+                       }
+
+
+                       System.out.println("\nNova arvore criada com sucesso!");
+                       System.out.println("\nArvore montada:");
+                       arvore.imprimirArvore();
+                   }
+                   break;
+
+
+               case "5":
+                   System.out.println("\nArvore atual:");
+                   arvore.imprimirArvore();
+                   break;
+
+
+               case "6":
+                   System.out.println("Ate mais!");
+                   sc.close();
+                   return;
+
+
+               default:
+                   System.out.println("Opcao invalida! Tente novamente.");
+           }
+       }
+   }
 }
